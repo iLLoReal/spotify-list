df16f388f28ed831fdf2ca152c24ab10b537537a
iLLoReal <olivier.laffon.dev@gmail.com>
2023-06-15 00:28:52 +0200

    Obtention de l'autorisation d'accès spotify, et obtention de l'access token

--- src/helpers/session.ts
+++ src/helpers/session.ts
@@ -1,14 +1,12 @@
-import { fetchBearerToken } from '../api/login.ts';
-import { BEARER_TOKEN_STORAGE_KEY } from '../globals.ts';
+import { fetchAuthorizationCode, fetchBearerToken } from '../api/login.ts';
+import { BEARER_TOKEN_STORAGE_KEY, CLIENT_ID } from '../globals.ts';
 
 export const getBearerToken = () => {
     return localStorage.getItem(BEARER_TOKEN_STORAGE_KEY);
 }
 
-export const logUserIn = async ({ username, password }: { username: string, password: string }) => {
-    console.log('DEBUG: call to spotify api with ', username, ' with password ', password);
-    const bearer_token = await fetchBearerToken(username, password);
-    localStorage.setItem(BEARER_TOKEN_STORAGE_KEY, bearer_token);
+export const requestPermission = async () => {
+    return await fetchAuthorizationCode(CLIENT_ID);
 }
 
 export const logUserOut = () => {
