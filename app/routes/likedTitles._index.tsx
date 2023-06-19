2ccd85e48767886430b1c6ac0ab08165a0b8c38d
iLLoReal <olivier.laffon.dev@gmail.com>
2023-06-19 17:48:02 +0200

    Fix mauvaise suppression loader/action

--- app/routes/likedTitles._index.tsx
+++ app/routes/likedTitles._index.tsx
@@ -1,6 +1,20 @@
+import { ActionArgs, LoaderArgs, redirect } from "@remix-run/node";
 import { ClientOnly } from "remix-utils";
 import Spinner from "~/components/Spinner";
 import Tracks from "~/components/Tracks";
+import { getBearerToken, logUserOut } from "~/helpers/session";
+
+export const action = async ({ request }: ActionArgs) => {
+    return await logUserOut(request);
+}
+
+export const loader = async ({ request }: LoaderArgs) => {
+    const bearerToken = await getBearerToken(request);
+    if (!bearerToken && process.env.NODE_ENV === "production") {
+        return redirect('/authorize');
+    }
+    return true;
+}
 
 export default function LikedTitles() {
     return (
