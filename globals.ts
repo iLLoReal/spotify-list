e4d87004bda78a64e6e9c07f8f2678dbf1bdd946
iLLoReal <olivier.laffon.dev@gmail.com>
2023-06-28 04:12:36 +0200

    fix: hostname du redirect pour connexion spotify

--- globals.ts
+++ globals.ts
@@ -3,7 +3,7 @@ export const BEARER_TOKEN_KEY = "bearerToken";
 export const CODE_VERIFIER_KEY = "code_verifier"
 
 // Redirect URI registered in spotify's app account, used for granting authorization.
-export const REDIRECT_URI = "http://localhost:3000/requestAccessToken";
+export const REDIRECT_URI = window.location.hostname + "/requestAccessToken";
 
 export const PLAYLIST_LIMIT = 20;
 
