f4ec7ffef614c6acdf6b6b6acb15ba4bc61a7c64
iLLoReal <olivier.laffon.dev@gmail.com>
2023-07-23 18:17:03 +0200

    Hotfix: netlify authorize, debug4

--- app/routes/authorize.tsx
+++ app/routes/authorize.tsx
@@ -52,7 +52,6 @@ export default function authorize() {
                     </button>
                 </div>
             </Form>
-            {loaderData ? <p>{loaderData}</p> : null}
         </div>
     )
 }
