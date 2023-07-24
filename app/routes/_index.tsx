f2860595b138862c40488568fb192f7d2c1f04b0
iLLoReal <olivier.laffon.dev@gmail.com>
2023-07-24 14:04:23 +0200

    Hotfix Netlify: checking session.codeVerifier value in index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,25 @@
 
-import { redirect } from "@remix-run/node";
-import { Form, V2_MetaFunction } from "@remix-run/react";
+import { LoaderArgs, redirect } from "@remix-run/node";
+import { Form, V2_MetaFunction, useLoaderData } from "@remix-run/react";
+import { getSession } from "~/session";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "spotify-list" }];
 };
+
+export const loader = async ({request}: LoaderArgs) => {
+  const session = await getSession(request.headers.get('Cookie'));
+  const codeVerifer = session.get('code_verifier');
+  return codeVerifer || null;
+}
 
 export const action = () => {
   return redirect('/authorize');
 }
 
 export default function Index() {
-
+  const data = useLoaderData();
+  console.log('codeVerifier: ', data);
   return (
     <div className="h-full 
     flex flex-col justify-center 
