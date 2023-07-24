7168199c8ab23937f8e841519c4440745d10c7cc
iLLoReal <olivier.laffon.dev@gmail.com>
2023-07-24 16:50:49 +0200

    debug: redirect_uri 2)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 
 import { redirect } from "@remix-run/node";
 import { Form, V2_MetaFunction } from "@remix-run/react";
+import { REDIRECT_URI } from "globals";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "spotify-list" }];
@@ -11,6 +12,7 @@ export const action = () => {
 }
 
 export default function Index() {
+  console.log('Redirect_uri: ', REDIRECT_URI);
   return (
     <div className="h-full 
     flex flex-col justify-center 
