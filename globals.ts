5f9eb045d74b012bf99e973d7905183a11b5dd87
iLLoReal <olivier.laffon.dev@gmail.com>
2023-07-24 16:34:42 +0200

    Fixed invalid subdomain, removed redirect_uri from new url

--- globals.ts
+++ globals.ts
@@ -5,10 +5,9 @@ export const CODE_VERIFIER_KEY = "code_verifier";
 // Redirect URI registered in spotify's app account, used for granting authorization.
 export const PROTOCOL =  process.env.NODE_ENV === 'production' ? "https://" : 'http://';
 export const HOSTNAME = process.env.NODE_ENV === 'production' ? process.env.HOSTNAME : 'localhost';
-export const SUBDOMAIN = process.env.NODE_ENV === 'production' ? ".netlify.app" : '';
 export const PORT = process.env.NODE_ENV === 'production' ?  '' : ':8888';
 
-export const REDIRECT_URI = PROTOCOL + HOSTNAME + SUBDOMAIN + PORT + "/requestAccessToken";
+export const REDIRECT_URI = PROTOCOL + HOSTNAME + PORT + "/requestAccessToken";
 export const PLAYLIST_LIMIT = 20;
 
 //Spotify authorization scope
