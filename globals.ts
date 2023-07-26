7c9bea97d4682cad021268615316f41c6d725e1f
iLLoReal <olivier.laffon.dev@gmail.com>
2023-07-26 14:53:50 +0200

    debug: manually setting uri (fix uri)

--- globals.ts
+++ globals.ts
@@ -8,7 +8,7 @@ export const HOSTNAME = process.env.NODE_ENV === 'production' ? process.env.HOST
 export const PORT = process.env.NODE_ENV === 'production' ?  '' : ':8888';
 
 //export const REDIRECT_URI = PROTOCOL + HOSTNAME + PORT + "/requestAccessToken";
-export const REDIRECT_URI = "https://lucky-dusk-892462.netlify.app/requestAccessToken";
+export const REDIRECT_URI = "https://64c116e4288a3569181d8814--lucky-dusk-892462.netlify.app/requestAccessToken";
 export const PLAYLIST_LIMIT = 20;
 
 //Spotify authorization scope
