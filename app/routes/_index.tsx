ef529234fb2ba408b712d424443351e14cb72f4f
iLLoReal <olivier.laffon.dev@gmail.com>
2023-07-27 14:12:42 +0200

    Debug: setting cookie before external redirect

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,23 @@
 
-import { redirect } from "@remix-run/node";
+import { ActionArgs, redirect } from "@remix-run/node";
 import { Form, V2_MetaFunction } from "@remix-run/react";
 import { REDIRECT_ENDPOINT } from "globals";
+import { commitSession, getSession } from "~/session";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "spotify-list" }];
 };
 
-export const action = () => {
-  return redirect('/authorize');
+export const action = async ({request}: ActionArgs) => {
+  // Tricks pour set le domain du cookie dynamiquement
+  const url = new URL(request.url);
+  const session = await getSession('Cookies');
+  session.set('loggedIn', true);
+  return redirect('/authorize', {
+    headers: {
+        "Set-Cookie": await commitSession(session)
+    }
+});
 }
 
 export default function Index() {
